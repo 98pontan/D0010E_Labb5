5a484761ea2bac5d53fd62543eabf8df66c2641b
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-01 20:18:42 +0100

    Update Customer.java

--- Laboration 5/src/lab5/store/Customer.java
+++ Laboration 5/src/lab5/store/Customer.java
@@ -2,6 +2,7 @@ package lab5.store;
 
 public class Customer {
   private int ID;
+  private CustomerState state;
   
   public Customer(int number) {
     this.ID = number;
@@ -13,5 +14,17 @@ public class Customer {
   
   public toString(){
     return String.valueOf(ID);
+  }
+  
+  public CustomerState getState(){
+    return state;
+  }
+  
+  public void setState(CustomerState state){
+    this.state = state;
+  }
+  
+  public class enum CustomerState {
+    TURNED_AWAY, LATE_CUSTOMER, IN_STORE, NOT_IN_STORE
   }
 }
