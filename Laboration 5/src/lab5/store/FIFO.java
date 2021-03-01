3ee6d522b55e621aa152f06d2dd37bda1fc638bd
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-01 19:29:36 +0100

    Update FIFO.java

--- Laboration 5/src/lab5/store/FIFO.java
+++ Laboration 5/src/lab5/store/FIFO.java
@@ -1,5 +1,57 @@
 package lab5.store;
+import java.util.ArrayList;
 
-public class FIFO {
+public class FIFO{
 
+	private ArrayList<Customer> FIFOQueue = new ArrayList<Customer>();
+	private int max = 0;
+	
+	public void add(Customer customer) {
+		FIFOQueue.add(customer);
+		
+		if(FIFOQueue.size() > max) {
+			max = FIFOQueue.size();
+		}
+	}
+
+	public Object first() throws NoSuchElementException {
+		if(FIFOQueue.size() == 0) {
+			throw new NoSuchElementException();
+		}
+		
+		return FIFOQueue.get(0);
+	}
+
+	public boolean isEmpty() {
+		if(FIFOQueue.size() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public int maxSize() {
+		return max;
+	}
+
+	public void removeFirst() throws NoSuchElementException {
+		if(FIFOQueue.size() == 0) {
+			throw new NoSuchElementException();
+		}
+		FIFOQueue.remove(0);
+	}
+
+	public int size() {
+		return FIFOQueue.size();
+	}
+	
+	public String toString() {
+		String s = "Queue: ";
+		
+		for(Object elem: FIFOQueue) {
+			s = s + "(" + String.valueOf(elem) + ") ";
+		}
+		
+		return s;
+	}
+	
 }
