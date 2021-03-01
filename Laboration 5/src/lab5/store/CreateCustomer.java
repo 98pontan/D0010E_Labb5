a750c53a9ec4fa179016c74320e88e1e4956f576
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-01 19:46:12 +0100

    Update CreateCustomer.java

--- Laboration 5/src/lab5/store/CreateCustomer.java
+++ Laboration 5/src/lab5/store/CreateCustomer.java
@@ -1,5 +1,11 @@
 package lab5.store;
 
 public class CreateCustomer {
-
+  private int createdCustomers = 0;
+  
+  public Customer CreateCustomer(){
+    Customer customer = new Customer(createdCustomers);
+    createdCustomers++;
+    return customer;
+  }
 }
