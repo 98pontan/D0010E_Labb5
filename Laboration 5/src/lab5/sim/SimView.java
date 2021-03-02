064ddaed89446de70d7108dadad140ba04f045da
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-02 18:39:19 +0100

    Update SimView.java

--- Laboration 5/src/lab5/sim/SimView.java
+++ Laboration 5/src/lab5/sim/SimView.java
@@ -1,5 +1,18 @@
 package lab5.sim;
+import java.util.Observable;
+import java.util.Observer;
+import lab5.SimState;
+import lab5.SimState.StoreState;
 
-public class SimView {
 
+public abstract class SimView implements Observer{
+  State simState;
+  State storeState;
+  
+  public SimView(State simState, State storeState){
+    simState.addObserver(this);
+    storeState.addObserver(this);
+  }
+  
+  abstract public void update(Observable o, Object arg);
 }
