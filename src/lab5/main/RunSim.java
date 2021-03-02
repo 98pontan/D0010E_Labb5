16946174aacb65a4a8ad56e296e9bed336daf1b3
Jesper Oscarsson <jesper@fullersta.com>
2021-03-02 17:18:53 +0100

    Initial code for starting a simulation added

--- src/lab5/main/RunSim.java
+++ src/lab5/main/RunSim.java
@@ -1,7 +1,30 @@
 package lab5.main;
 
+import lab5.events.StartEvent;
+import lab5.events.StopEvent;
+import lab5.sim.EventQueue;
+import lab5.sim.SimState;
+import lab5.sim.SimView;
+import lab5.sim.Simulator;
+import lab5.store.StoreState;
+import lab5.view.StoreView;
+
 public class RunSim {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        final int REGISTERS         = 3;
+        final int MAX_CUSTOMERS     = 30;
+        // How many customers that arrive per time unit
+        final int CUSTOMER_INTERVAL = 4;
+        final int SIM_TIME          = 10;
+        final int SEED              = 9999;
+
+        SimState model = new StoreState();
+        SimView view = new StoreView(/*model*/);
+        EventQueue queue = new EventQueue();
+        queue.add(new StartEvent(/*SEED, REGISTERS, MAX_CUSTOMERS, CUSTOMER_INTERVAL*/));
+        queue.add(new StopEvent(/*SIM_TIME*/));
+
+        Simulator sim = new Simulator(model, view, queue);
+        sim.run();
     }
 }
--- src/lab5/sim/Simulator.java
+++ src/lab5/sim/Simulator.java
@@ -1,5 +1,20 @@
 package lab5.sim;
 
 public class Simulator {
+    SimState model;
+    SimView view;
+    EventQueue queue;
+    private boolean simRunning = true;
 
+    public Simulator(SimState model, SimView view, EventQueue queue) {
+        this.model = model;
+        this.view = view;
+        this.queue = queue;
+    }
+
+    public void run() {
+        while (simRunning) {
+            queue.getNext().execute();
+        }
+    }
 }
