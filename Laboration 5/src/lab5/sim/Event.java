8df0a9494108f6a9543f716afaec2827c37f3094
Pontus Jirbratt <pontus.jirbratt@hotmail.com>
2021-03-02 15:48:47 +0100

    first version of EventQueue, SortedSequence, SimState and StoreState

--- Laboration 5/src/lab5/sim/Event.java
+++ Laboration 5/src/lab5/sim/Event.java
@@ -1,5 +1,13 @@
 package lab5.sim;
 
+import java.util.Random;
+
 public class Event {
+	double time;
+	
+	Event(){
+		Random r = new Random();
+		time = r.nextDouble();
+	}
 
 }
--- Laboration 5/src/lab5/sim/SimState.java
+++ Laboration 5/src/lab5/sim/SimState.java
@@ -1,5 +1,39 @@
 package lab5.sim;
 
-public class SimState {
+import java.util.Observable;
 
+import lab5.store.StoreState;
+/**
+ * is the general state of the simulation
+ * @author Pontus Eriksson Jirbratt
+ *
+ */
+public class SimState extends Observable
+{
+	double currentTime;
+	int numberOfEvents = 0;
+	boolean runFlag;
+	
+	SimState(int openCheckouts, int customerLimit)
+	{
+		currentTime = 0;
+		runFlag = true;
+	}
+	void update(double time) 
+	{
+		setChanged();
+		notifyObservers();
+	}
+	
+	boolean getSimRunning() 
+	{
+		
+		return runFlag;
+	}
+	
+	double getcurrentTime()
+	{
+		return currentTime;
+	}
+	
 }
