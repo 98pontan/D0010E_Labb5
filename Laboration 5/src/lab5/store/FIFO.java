66a475fa2d7c952ffac2f931e5bdbc409190b722
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-02 18:41:59 +0100

    Update FIFO.java

--- Laboration 5/src/lab5/store/FIFO.java
+++ Laboration 5/src/lab5/store/FIFO.java
@@ -22,23 +22,25 @@ public class FIFO{
 		
 		return FIFOQueue.get(0);
 	}
-
+	
+	public void removeFirst() throws NoSuchElementException {
+		if(FIFOQueue.size() == 0) {
+			throw new NoSuchElementException();
+		}
+		
+		FIFOQueue.remove(0);
+	}
+	
 	public boolean isEmpty() {
 		if(FIFOQueue.size() == 0) {
 			return true;
 		}
+		
 		return false;
 	}
 
 	public int maxSize() {
 		return max;
-	}
-
-	public void removeFirst() throws NoSuchElementException {
-		if(FIFOQueue.size() == 0) {
-			throw new NoSuchElementException();
-		}
-		FIFOQueue.remove(0);
 	}
 
 	public int size() {
