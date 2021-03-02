c742e2df6b72c477447bf8a0f65f9f7dc03e3060
Jesper Oscarsson <jesper@fullersta.com>
2021-03-02 20:23:24 +0100

    Initial code for Time class

--- src/lab5/store/Time.java
+++ src/lab5/store/Time.java
@@ -1,5 +1,35 @@
 package lab5.store;
 
-public class Time {
+import lab5.random.*;
+import lab5.sim.SimState;
 
+public class Time {
+    private UniformRandomStream uniRegisterRnd;
+    private UniformRandomStream uniGatherRnd;
+    private ExponentialRandomStream expArrivalRnd;
+
+    public Time(
+            long SEED,
+            double lowerRegister,
+            double upperRegister,
+            double lowerGather,
+            double upperGather,
+            double lambda
+    ) {
+        uniRegisterRnd  = new UniformRandomStream(lowerRegister,upperRegister, SEED);
+        uniGatherRnd    = new UniformRandomStream(lowerGather,upperGather, SEED);
+        expArrivalRnd   = new ExponentialRandomStream(lambda, SEED);
+    }
+
+    public double generateRegisterTime() {
+        return SimState.getCurrentTime() + uniRegisterRnd.next();
+    }
+
+    public double generateGatherTime() {
+        return SimState.getCurrentTime() + uniGatherRnd.next();
+    }
+
+    public double generateArrivalTime() {
+        return SimState.getCurrentTime() + expArrivalRnd.next();
+    }
 }
