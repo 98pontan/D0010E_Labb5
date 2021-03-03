38d25394c6bca39765506c4804c4b59016cd71ff
Jesper Oscarsson <jesper@fullersta.com>
2021-03-03 10:17:23 +0100

    Updated RunSim StartEvent arguments with constants for use in Time class.

--- src/lab5/main/RunSim.java
+++ src/lab5/main/RunSim.java
@@ -11,17 +11,29 @@ import lab5.view.StoreView;
 
 public class RunSim {
     public static void main(String[] args) {
+        final int SEED              = 9999;
         final int REGISTERS         = 3;
         final int MAX_CUSTOMERS     = 30;
-        // How many customers that arrive per time unit
-        final int CUSTOMER_INTERVAL = 4;
+        // Simulation run time in time units
         final int SIM_TIME          = 10;
-        final int SEED              = 9999;
+        // How many customers that arrive per time unit
+        final double ARRIVAL_SPEED  = 4;
+        // Lower and upper limits in time generation for
+        // corresponding events
+        final double lowerGather    = 1, upperGather = 2;
+        final double lowerRegister  = 1, upperRegister = 2;
 
         SimState model = new StoreState();
         SimView view = new StoreView(/*model*/);
         EventQueue queue = new EventQueue();
-        queue.add(new StartEvent(/*SEED, REGISTERS, MAX_CUSTOMERS, CUSTOMER_INTERVAL*/));
+        queue.add(new StartEvent(/*
+                SEED,
+                REGISTERS,
+                MAX_CUSTOMERS,
+                ARRIVAL_SPEED,
+                lowerGather, upperGather,
+                lowerRegister, upperRegister
+        */));
         queue.add(new StopEvent(/*SIM_TIME*/));
 
         Simulator sim = new Simulator(model, view, queue);
