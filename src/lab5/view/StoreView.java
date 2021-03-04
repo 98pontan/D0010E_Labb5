00ec278af3079202c21c1c2bba523e984683e334
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-04 19:13:20 +0100

    Update StoreView.java

--- src/lab5/view/StoreView.java
+++ src/lab5/view/StoreView.java
@@ -4,32 +4,54 @@ import lab5.sim.SimState;
 import lab5.store.*;
 import lab5.events.*;
 
+/**
+ * This class prints out the data for the simulation of the store.
+ * It prints out the start followed by the events that is called and then the end.
+ * @author Markus Blomqvist
+ *
+ */
+
 public class StoreView extends SimView{
   private SimState simState;
   private StoreState storeState;
   
+/**
+ * This is the constructor for this class.
+ */
   public StoreView(SimState simState, StoreState storeState){
     super(simState,storeState);
     this.simState = simState;
     this.storeState = storeState;
   }
   
+/**
+ * This method prints out the start.
+ */
   private void printStart(){
     System.out.println("PARAMETRAR");
     System.out.println("==========");
     System.out.println("Antal kassor, N..........: ") + storeState.getcheckOuts();
     System.out.println("Max som ryms, M..........: " + storeState.getMaxCustomersToday());
     System.out.println("Ankomsthastighet, lambda.: ") + ;
-    System.out.println("Plocktider, [P_min..Pmax]: ") + ;
-    System.out.println("Betaltider, [K_min..Kmax]: ") + ;
+    System.out.println("Plocktider, [P_min..Pmax]: ") + "[" + + ".." + + "]";
+    System.out.println("Betaltider, [K_min..Kmax]: ") + "[" + + ".." + + "]";
     System.out.println("Frö, f...................: ") + ;
     System.out.println("");
+    System.out.println("FÖRLOPP");
+    System.out.println("=======");
+    System.out.println("Tid\tHändelse\tKund\t?\tled\tledT\tI\t$\t:-(\tköat\tköT\tköar\t[Kassakö..]");
   }
   
+/**
+ * This method prints out the events.
+ */
   private void printEvents(){
     System.out.println("");
   }
   
+/**
+ * This method prints out the end.
+ */
   private void printEnd(){
     System.out.println("");
     System.out.println("RESLUTAT");
@@ -43,9 +65,39 @@ public class StoreView extends SimView{
     System.out.println("3) Total tid " + + " kunder tvingats köa: " + + " te.");
     System.out.println("   Genomsnittlig kötid: " + + " te.");
   }
+
+/**
+ * This method makes sure that the time has the correct format
+ * to print out in the simulation with 2 decimals.
+ * @param time
+ * @return
+ */
+  private String timeFormat(double time){
+    String printTime = String.valueOf(Math.round(time * 100.0) / 100.0);
+       if(printTime.length() < 4){
+           printTime = printTime + "0";
+       }
+       return printTime;
+  }
   
+/**
+ * This method updates the printing depending on what
+ * the current event is.
+ * 
+ */
   public void update(Observable o, Object arg){
-    
+    if (simState.getCurrentEvent().getClass() == StartEvent.class)
+      {
+         printStart();
+         System.out.println(simState.getCurrentTime() + "\tStart");
+      } else if (simState.getCurrentEvent().getClass() == StopEvent.class)
+      {
+         System.out.println(timeFormat(simState.getCurrentTime()) + "\tStop");
+         printEnd();
+      } else
+      {
+         printEvents();
+      }
   }
   
 }
