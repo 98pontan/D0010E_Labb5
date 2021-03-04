719b4b041e8ed1c50d2f7f8a7216fe1278b0d0f9
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-04 19:33:37 +0100

    Update Customer.java

--- src/lab5/store/Customer.java
+++ src/lab5/store/Customer.java
@@ -1,44 +1,44 @@
 package lab5.store;
 
 /**
- 	* This class represents a customer with ID and a state.
+  * This class represents a customer with ID and a state.
   * @author Markus Blomqvist
- 	*/
+  */
 public class Customer {
   private int ID;
   private CustomerState state;
   
  /**
- 	* This constructor creates a customer with an ID.
- 	*/
+  * This constructor creates a customer with an ID.
+  */
   public Customer(int number) {
     this.ID = number;
   }
   
  /**
- 	* This method returns the ID of the customer.
- 	*/
+  * This method returns the ID of the customer.
+  */
   public int getID(){
     return ID;
   }
   
  /**
- 	* This method returns a String of the ID of the customer.
- 	*/
+  * This method returns a String of the ID of the customer.
+  */
   public String toString(){
     return String.valueOf(ID);
   }
   
  /**
- 	* This method returns the current state of the customer.
- 	*/
+  * This method returns the current state of the customer.
+  */
   public CustomerState getState(){
     return state;
   }
   
  /**
- 	* This method sets the state of the customer.
- 	*/
+  * This method sets the state of the customer.
+  */
   public void setState(CustomerState state){
     this.state = state;
   }
