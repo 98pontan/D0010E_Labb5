57395082360c7c3f0f7dcf2d98562684f670236c
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-04 19:35:31 +0100

    Update CustomerState.java

--- src/lab5/store/CustomerState.java
+++ src/lab5/store/CustomerState.java
@@ -1,5 +1,9 @@
 package lab5.store;
 
+ /**
+  * This class represents the diffrent states a of customer.
+  * @author Markus Blomqvist
+  */
 public enum CustomerState {
   TURNED_AWAY, LATE_CUSTOMER, IN_STORE, NOT_IN_STORE
 }
