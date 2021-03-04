ff98425f1562f00d81cadfe808c23906ba9a6658
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-04 19:39:55 +0100

    Update CreateCustomer.java

--- src/lab5/store/CreateCustomer.java
+++ src/lab5/store/CreateCustomer.java
@@ -1,8 +1,17 @@
 package lab5.store;
 
+/**
+  * This class creates customers and automatically increases the
+  * number of customers created.
+  * @author Markus Blomqvist
+  */
 public class CreateCustomer {
   private int createdCustomers = 0;
   
+ /**
+  * This constructor creates a customer with an ID of the number of customers created.
+  * The constructor also returns a customer.
+  */
   public Customer createCustomer(){
     Customer customer = new Customer(createdCustomers);
     createdCustomers++;
