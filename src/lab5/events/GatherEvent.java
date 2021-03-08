50d6160fba70c7cc3e4080dc09ccbeb900d24662
Jesper Oscarsson <jesper@fullersta.com>
2021-03-08 10:07:18 +0100

    Created code for GatherEvent

--- src/lab5/events/GatherEvent.java
+++ src/lab5/events/GatherEvent.java
@@ -1,6 +1,9 @@
 package lab5.events;
-import lab5.sim.*;
-import lab5.store.*;
+
+import lab5.store.StoreState;
+import lab5.store.Customer;
+import lab5.sim.Event;
+import lab5.sim.EventQueue;
 
 /**
  * Description
@@ -12,35 +15,30 @@ import lab5.store.*;
  *
  */
 public class GatherEvent extends Event {
-	
-	private PurchaseEvent payEvent;
-	private double payTime;
-	
-	public GatherEvent(SimState state, EventQueue eventQueue, Customer customer, double time)
-	{
+	public GatherEvent(StoreState state, EventQueue eventQueue, Customer customer, double executionTime) {
 		super(state, eventQueue);
-		this.time = time;
+		this.time = executionTime;
 		this.name = "Gather";
 		this.customer = customer;
 	}
 	
-	public void run() 
-	{
-		state.update(this);
-//			      
-//		if(state.getCurrentSim().getFreeRegisters() > 0)
-//		{
-//			double payTime = this.time + state.getPayTime().next();
-//				         
-//			payEvent = new PurchaseEvent(this.state, this.eventQueue, payTime, customer);
-			eventQueue.addEvent(payEvent);
-//			state.getCurrentSim().occupieRegister();
-//		}
-//		else 
-//		{
-//			state.getStore().getFIFOQueue().add(customer);
-//			state.getStore().addTotalCustomersInQueue();
-//		}
+	public void run() {
+		StoreState model = (StoreState) this.state;
+		if (model.checkAvailableCheckout()) {
+			model.closeCheckouts(1);
+			eventQueue.addEvent(new PurchaseEvent(model, eventQueue, customer, 0.0));
+		}
+
+		else {
+			model.addToQueue(customer);
+		}
+
+		_updateTime(model);
+		model.update();
+	}
+
+	private void _updateTime(StoreState model) {
+		model.setTime(this.time);
 	}
 	
 	public double getTime()
