1efaaad2a8855a14f9ef992768104a6193b76140
Marmak95 <79858654+Marmak95@users.noreply.github.com>
2021-03-09 21:02:35 +0100

    Update Optimize.java

--- src/lab5/main/Optimize.java
+++ src/lab5/main/Optimize.java
@@ -1,18 +1,29 @@
 package lab5.main;
 import java.util.Random;
-import sim.SimState;
+import lab5.events.StartEvent;
+import lab5.events.ClosingEvent;
+import lab5.events.StopEvent;
+import lab5.sim.*;
+import lab5.store.StoreState;
 
+/**
+ * This class is used to optimize the 
+ * @author Markus Blomqvist.
+ */
 public class Optimize {
   public static void main(String[] args) {
     Optimize opt = new Optimize();
-    long seed = 0;
-    System.out.println(opt.metod2(seed));
+    long SEED = 315354808530047762;
+    System.out.println(opt.metod2(SEED));
   }
   
   public int metod1(long SEED, int CHECKOUTS, , int MAX_CUSTOMERS,
                    double lowerGather, double upperGather, double ARRIVAL_TIME,
                    double lowerRegister, double upperRegister,  double SIM_TIME){
     
+    // Kanske ska skapa en SimState model istället? Kanske fråga om detta?
+    // I lab5 dokumentet: "Viktigt är att den generella simulatorn inget vet om den specifika."
+    // "Den ska fungera för vilken specifik simulator som helst, inte bara en snabbköpssimulator."
     StoreState model = new StoreState(
             SEED,
             CHECKOUTS,
@@ -29,14 +40,70 @@ public class Optimize {
     Simulator sim = new Simulator(model, queue);
     sim.run();
     
-    return model; //sluttillståndet ska returneras
+    return model.getMissedCustomers();
   }
   
   public int metod2(long SEED){
+    // The first method's parameters.
+    int MAX_CUSTOMERS = 6;
+    double SIM_TIME = 10;
+    double ARRIVAL_TIME = 1;
+    double lowerGather = 0.5;
+    double upperGather = 0.9;
+    double lowerRegister = 2;
+    double upperRegister = 4;
     
+    // The minimal amount of checkouts can not be bigger than MAX_CUSTOMERS.
+    int minCheckouts = MAX_CUSTOMERS;
+    
+    // Initial value of missedCustomers.
+    int missedCustomers = metod1(long SEED, int CHECKOUTS, , int MAX_CUSTOMERS,
+                                 double lowerGather, double upperGather, double ARRIVAL_TIME,
+                                 double lowerRegister, double upperRegister,  double SIM_TIME);
+    
+    // Gets a new value as long as minCheckouts >= 1.
+    while(minCheckouts >= 1){
+      // Creates a new amount of missed customers.
+      int newMissedCustomers = metod1(long SEED, int minCheckouts, , int MAX_CUSTOMERS,
+                                      double lowerGather, double upperGather, double ARRIVAL_TIME,
+                                      double lowerRegister, double upperRegister,  double SIM_TIME);
+      
+      // Checks if NewMissedCustomers is different than the inital value.
+      // If it is different the last amount of checkouts was the most opmtimal amount.
+      if(missedCustomers != newMissedCustomers){
+        return minCheckouts + 1;
+      }
+      
+      // Decreases the amount of minCheckouts.
+      minCheckouts--;
+    }
+    
+    return MAX_CUSTOMERS;
   }
   
-  public int metod3(){
+  public int metod3(long SEED){
+    // Sets a random seed number.
+    Random rand = new Random(SEED);
     
+    int maxMinCheckouts = 0;
+    int counter = 0;
+    int checkBigger;
+    
+    // Runs 100 times if the maximum of the minimum number of checkouts has not changed.
+    while(counter < 100){
+      int newAmountOfCheckouts = metod2(rand.nextLong());
+      
+      // If true, the counter resets.
+      if(maxMinCheckouts != (checkBigger = maxMinCheckouts > newAmountOfCheckouts ? newAmountOfCheckouts : maxMinCheckouts)){
+        counter = 0;
+      }
+      else{
+        counter++;
+      }
+      
+      maxMinCheckouts = (checkBigger = maxMinCheckouts > newAmountOfCheckouts ? newAmountOfCheckouts : maxMinCheckouts);
+    }
+    
+    return maxMinCheckouts;
   }
 }
